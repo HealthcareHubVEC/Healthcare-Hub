bc74da05f96d1a3077eb1c95cec66cb033520825
Swaroop <sswaru0210@gmail.com>
2024-04-17 22:01:32 +0530

    faq done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -280,18 +280,6 @@ const Home = () => {
       {/*======== faq section end ========*/}
 
       {/*======testimonial========*/}
-      <section>
-        <div className="container">
-        <div className="xl:w-[470px] mx-auto">
-            <h2 className="heading text-center">What our patient say</h2>
-            <p className="text__para text-center">
-              World-class care for everyone. Our health System offers unmatched,
-              expert health care.
-            </p>
-          </div>
-          <Testimonial />
-        </div>
-      </section>
       {/*======testimonial end======*/}
     
     </>
