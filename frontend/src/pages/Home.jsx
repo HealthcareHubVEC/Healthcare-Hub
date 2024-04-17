fefad35ef6f8ea500774a82f33e744357cf47d8d
Swaroop <sswaru0210@gmail.com>
2024-04-18 00:54:29 +0530

    footer update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -15,6 +15,7 @@ import About from "../components/About/About";
 import ServiceList from "../components/Services/ServiceList";
 import DoctorList from "../components/Doctors/DoctorList";
 import FaqList from "../components/Faq/FaqList";
+
 const Home = () => {
   return <>
     {/*===== hero section =====*/}
@@ -280,6 +281,18 @@ const Home = () => {
       {/*======== faq section end ========*/}
 
       {/*======testimonial========*/}
+      {/*<section>
+        <div className="container">
+          <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our patient say</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+
+        </div>
+      </section>*/}
       {/*======testimonial end======*/}
     
     </>
