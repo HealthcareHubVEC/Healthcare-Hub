319b70fb80aa58426a02a2dc38e056d7a2ece8bf
Swaroop <sswaru0210@gmail.com>
2024-04-17 18:25:49 +0530

    testimonial...

---
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+import { Pagination } from 'swiper'
+import {Swiper, Swiper} from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagintion'
+import patientAvatar from '../../assets/images/patient-avatar.png'
+import {HiStar} from 'react-icons'
+
+const Testimonial = () => {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -278,6 +278,19 @@ const Home = () => {
         </div>
       </section>
       {/*======== faq section end ========*/}
+      {/*======testimonial========*/}
+      <section>
+        <div className="container">
+        <div className="xl:w-[470px] mx-auto">
+            <h2 className="heading text-center">What our patient say</h2>
+            <p className="text__para text-center">
+              World-class care for everyone. Our health System offers unmatched,
+              expert health care.
+            </p>
+          </div>
+        </div>
+      </section>
+      {/*======testimonial end======*/}
     
     </>
   </>
