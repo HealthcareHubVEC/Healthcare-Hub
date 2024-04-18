3e8b69d02b0f4cde3927e9383c9981285f3b62a2
AnaghaTantry <tantryanagha@gmail.com>
2024-04-18 16:18:15 +0530

    Feedback Doctor details Feedback

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { formatDate } from '../../utils/formatDate';
 
 const DoctorAbout = () => {
@@ -25,7 +24,7 @@ const DoctorAbout = () => {
                 <li className = "flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] ">
                     <div>
                         <span className = "text-irisBlueColor text-[15px] leading-6 font-semibold">
-                            23 June, 2008
+                        {formatDate('09-13-2014')} - {formatDate('09-13-2016')}
                             </span>
                             <p className="text-[16px] leading-6 font-medium text-textColor">
                                 PHD in Surgeon
@@ -38,7 +37,7 @@ const DoctorAbout = () => {
                 <li className = "flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px] ">
                     <div>
                         <span className = "text-irisBlueColor text-[15px] leading-6 font-semibold">
-                            {formatDate('12-04-2010')}
+                            {formatDate('07-04-2010')} - {formatDate('09-13-2014')}
                             </span>
                             <p className="text-[16px] leading-6 font-medium text-textColor">
                                 PHD in Surgeon
@@ -50,6 +49,38 @@ const DoctorAbout = () => {
                 </li>
 
             </ul>
+        </div>
+        <div className="mt-12">
+        <h3 className = "text-[20px] leading-[30px] text-headingColor font-semibold">
+                Experience
+            </h3>
+            <ul className = "grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5">
+                <li className="p-4 rounded bg-[#fff9ea]">
+                    <span className = "text-yellowColor text-[15px] leading-6" font-semibold>
+                    {formatDate('07-04-2010')} - {formatDate('09-13-2014')}      
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">
+                                Sr. Surgeon
+                            </p>
+                    <p className="text-[14px] leading-5 font-medium text-textColor">
+                            New Apollo Hospital, New York.
+                    </p>
+
+                </li>
+                <li className="p-4 rounded bg-[#fff9ea]">
+                    <span className = "text-yellowColor text-[15px] leading-6" font-semibold>
+                    {formatDate('07-04-2010')} - {formatDate('09-13-2014')}      
+                    </span>
+                    <p className="text-[16px] leading-6 font-medium text-textColor">
+                                Sr. Surgeon
+                            </p>
+                    <p className="text-[14px] leading-5 font-medium text-textColor">
+                            New Apollo Hospital, New York.
+                    </p>
+
+                </li>
+            </ul>
+
         </div>
 
   </div>
--- frontend/src/pages/Doctors/DoctorDetails.jsx
+++ frontend/src/pages/Doctors/DoctorDetails.jsx
@@ -3,6 +3,7 @@ import doctorImg from '../../assets/images/doctor-img02.png'
 import starIcon from '../../assets/images/Star.png'
 import DoctorAbout from './DoctorAbout';
 import Feedback from './Feedback';
+import SidePanel from './SidePanel';
 
 const DoctorDetails = () => {
   const [tab,setTab] = useState('about')
@@ -57,11 +58,12 @@ const DoctorDetails = () => {
       <div className="mt-[50px]">
         {tab==='about' && <DoctorAbout />}
         {tab==='feedback' && <Feedback />}
-
       </div>
       </div>
 
-        <div></div>
+        <div>
+          <SidePanel />
+        </div>
       </div>
     </div>
   </section>
