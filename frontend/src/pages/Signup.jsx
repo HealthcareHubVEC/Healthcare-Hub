3c09ae191ea8b199badb459e4975c125501b3b6b
AnaghaTantry <tantryanagha@gmail.com>
2024-04-23 12:42:56 +0530

    register linking

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import signupImg from "../assets/images/signup.gif";
 import avatar from "../assets/images/doctor-img01.png";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../config.js';
+import { toast } from 'react-toastify';
+import HashLoader from 'react-spinners/HashLoader';
 
 const Signup = () => {
     const [selectedFile, setSelectedFile] = useState(null);
     const [previewURL, setPreviewURL] = useState("");
+    const[loading, setLoading] = useState(false)
     const [formData, setFormData] = useState({
         name: "",
         email: "",
@@ -14,6 +18,8 @@ const Signup = () => {
         gender: "",
         role: "patient",
       });
+
+      const navigate = useNavigate();
     
       const handleInputChange = e=>{
         setFormData({... formData, [e.target.name]: e.target.value});
@@ -28,7 +34,28 @@ const Signup = () => {
       };
 
       const submitHandler = async event=>{
-        event.preventDefault()
+        event.preventDefault();
+        setLoading(true)
+        try {
+            const res = await fetch(`${BASE_URL}/auth/register`,{
+                method: 'post',
+                headers: {
+                    'Content-Type':'application/json'
+                },
+                body: JSON.stringify(formData)
+            })
+            const { message } = await res.json()
+            if(!res.ok){
+                throw new Error(message)
+            } 
+            setLoading(false)
+            toast.success(message)
+            navigate('/login')
+            
+        } catch (err) {
+            toast.error(err.message)
+            setLoading(false)
+        }
       };
 
     return (
@@ -119,10 +146,11 @@ const Signup = () => {
 
 
                             <div className="mb-5 flex items-center gap-3">
+                               { selectedFile &&
                                 <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor
                                 flex items-center justify-center">
-                                    <img src={avatar} alt="" className="w-full rounded-full" />
-                                </figure>
+                                    <img src={previewURL} alt="" className="w-full rounded-full" />
+                                </figure> }
 
                                 <div className="relative w-[160px] h-[50px]">
                                     <input
@@ -145,11 +173,16 @@ const Signup = () => {
 
                             <div className="mt-7">
                                 <button 
+                                disabled =  {loading && true}
                                     type="submit" 
                                     className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4
                                     py-3" 
                                 >
-                                    Sign Up
+                                    { loading ?( 
+                                    <HashLoader size={35} color= "#ffffff"/>
+                                    ):(
+                                    'Sign Up'
+                                    )}
                                 </button>
                             </div>
                             <p className="mt-5 text-textColor text-center">
