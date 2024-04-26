d881b6247ca3d8234452ed9ff0fe7913931a091a
Swaroop <sswaru0210@gmail.com>
2024-04-26 12:04:18 +0530

    Doc_profile...

---
+++ frontend/src/Dashboard/doctor-account/Profile.jsx
@@ -0,0 +1,20 @@
+
+
+const Profile = () => {
+    return (
+        <div>
+            <h2 className="text-headingColor font-bold text-[24px] leading-9 mb-10">
+                Profile Informations
+            </h2>
+
+            <form>
+                <div className="mb-5">
+                    <p className="form__label">Name*</p>
+                    <input />
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Profile
